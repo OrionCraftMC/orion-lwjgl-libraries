1ced67a353a622b7575d6e57e40efd8ec26449b0
NickAcPT <32451103+NickAcPT@users.noreply.github.com>
2021-11-08 15:43:55 +0000

    Add libraries

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    java
+    id("com.github.johnrengelman.shadow") version "7.1.0"
+}
+
+group = "io.github.orioncraftmc"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+tasks {
+    build.get().dependsOn(shadowJar)
+    shadowJar {
+        archiveClassifier.set(null as String?)
+
+        arrayOf("org.lwjgl").forEach {
+            relocate(it, "${project.group}.libs.$it")
+        }
+    }
+}
+
+dependencies {
+    implementation(platform("org.lwjgl:lwjgl-bom:3.2.3"))
+
+    implementation("org.lwjgl", "lwjgl")
+    implementation("org.lwjgl", "lwjgl-nanovg")
+    implementation("org.lwjgl", "lwjgl-yoga")
+
+    listOf("windows", "linux", "macos").forEach { osName ->
+        val nativeClassifier = "natives-$osName"
+
+        runtimeOnly("org.lwjgl", "lwjgl", classifier = nativeClassifier)
+        runtimeOnly("org.lwjgl", "lwjgl-nanovg", classifier = nativeClassifier)
+        runtimeOnly("org.lwjgl", "lwjgl-yoga", classifier = nativeClassifier)
+    }
+}
